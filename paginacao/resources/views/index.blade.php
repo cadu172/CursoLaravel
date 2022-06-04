20ed3b1540ca9f70dce12a16370dd91f6e137719
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2022-06-04 18:44:05 -0300

    Iniciando o capitulo 19 - paginação

--- paginacao/resources/views/index.blade.php
+++ paginacao/resources/views/index.blade.php
@@ -3,13 +3,15 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{csrf_token()}}">
-        <title>Laravel</title>
-        <style rel="stylesheet" src="{{asset('css/app.css')}}"></style>
+        <meta name="csrf-token" content="{{csrf_token}}">
+        <title>Paginação</title>
+
+        <style>
+        </style>
     </head>
-    <body>
+    <body>        
         <div class="container">
-            <h1>TESTE</h1>
+            <h1>Ola</h1>
         </div>
         <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
     </body>
