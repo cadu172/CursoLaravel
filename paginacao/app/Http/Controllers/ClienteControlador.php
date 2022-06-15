c76bc0a202cef99ca3bda73a456f6d8042c4c345
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2022-06-15 07:45:06 -0300

    arquivo package.json refeito

--- paginacao/app/Http/Controllers/ClienteControlador.php
+++ paginacao/app/Http/Controllers/ClienteControlador.php
@@ -15,11 +15,7 @@ class ClienteControlador extends Controller
     public function index()
     {
         //
-        $clientes = Clientes::all();
-        //$clientes = Clientes::paginate(10);
-
-        //dd($clientes);
-
+        $clientes = Clientes::paginate(10);
         // retorna a view com os dados
         return view('index',
             ['clientes'=>$clientes]);
