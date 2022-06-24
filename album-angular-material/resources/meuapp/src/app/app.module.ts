8f8924950537b5f2351ce29ebbb5104517b3b2f6
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2022-06-24 08:21:27 -0300

    Continuar na aula: 205. Instalação do Angular Material - Falta finalizar a aula

--- album-angular-material/resources/meuapp/src/app/app.module.ts
+++ album-angular-material/resources/meuapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +9,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+** procurar no site do material como incluir o arquivo css que vem no angular
+** parei na aula 205
