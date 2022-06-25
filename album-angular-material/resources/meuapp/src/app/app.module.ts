61325084038e6494cab98093d80c89e6071c586f
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2022-06-25 12:49:03 -0300

    Projeto albuim de fotos com amgular: 205. Instalação do Angular Material - CONCLUIDO

--- album-angular-material/resources/meuapp/src/app/app.module.ts
+++ album-angular-material/resources/meuapp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -17,7 +17,3 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-** procurar no site do material como incluir o arquivo css que vem no angular
-** parei na aula 205
--- album-angular-material/resources/views/index.blade.php
+++ album-angular-material/resources/views/index.blade.php
@@ -4,11 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel com Angular+Material</title>
+        <link rel="stylesheet" href="{{asset('js/styles.css')}}">
     </head>
     <body class="antialiased">
         <app-root></app-root>
-        <script src="{{asset('js/runtime.e52a78f68d3a501f.js')}}" type="module"></script>
-        <script src="{{asset('js/polyfills.4a877794f0c37d8b.js')}}" type="module"></script>
-        <script src="{{asset('js/main.c3a5220bf6c060d5.js')}}" type="module"></script>
+        <script src="{{asset('js/runtime.js')}}" type="module"></script>
+        <script src="{{asset('js/polyfills.js')}}" type="module"></script>
+        <script src="{{asset('js/main.js')}}" type="module"></script>
     </body>
 </html>
