88d74aa04552d7d3bcb4fa138b1e7f8979a164cd
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2022-07-03 23:04:13 -0300

    Concluído até a aula 215, testes OK, continuar na aula 216

--- album-angular-material/resources/meuapp/src/app/app.component.ts
+++ album-angular-material/resources/meuapp/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Post } from './post';
+import { PostDialogComponent } from './post-dialog/post-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,9 @@ import { Post } from './post';
 })
 
 export class AppComponent {
+  
   title = 'meuapp';
+  
   public posts: Post[] = [
     new Post("Carlos Eduardo","Cachorro Shiba","Cachorro Do Filme","cadu172@gmail.com","Estou fazendo o teste do texto que vai na mensagem do card"),
     new Post("Carlos Eduardo","Cachorro Shiba","Cachorro Do Filme","cadu172@gmail.com","Estou fazendo o teste do texto que vai na mensagem do card"),
@@ -19,4 +23,22 @@ export class AppComponent {
     new Post("Carlos Eduardo","Cachorro Shiba","Cachorro Do Filme","cadu172@gmail.com","Estou fazendo o teste do texto que vai na mensagem do card"),
     new Post("Carlos Eduardo","Cachorro Shiba","Cachorro Do Filme","cadu172@gmail.com","Estou fazendo o teste do texto que vai na mensagem do card")
   ];
+
+  constructor(public dialog: MatDialog) {
+
+  }
+
+  openDialog() {
+    
+    const dialogRef = this.dialog.open(PostDialogComponent, {
+      width: '400px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if ( result ) {
+        console.log(result);
+      }
+    });    
+
+  }
 }
--- album-angular-material/resources/meuapp/src/app/post-dialog/post-dialog.component.ts
+++ album-angular-material/resources/meuapp/src/app/post-dialog/post-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-post-dialog',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDialogComponent implements OnInit {
 
-  constructor() { }
+  public nomeArquivo = "";
 
-  ngOnInit(): void {
+  public dados = {
+    post : new Post("","","","",""),
+    arquivo : null
+  };
+
+  constructor ( public dialogRef: MatDialogRef<PostDialogComponent> ) { }
+
+  ngOnInit(): void { }
+
+  selecionarArquivo(event: any) {    
+    this.nomeArquivo = event.target.files[0].name;
+    this.dados.arquivo = event.target.files[0];
+  }
+
+  salvar() {
+    this.dialogRef.close(this.dados);
+  }
+
+  cancelar() {
+    this.dialogRef.close(null);
   }
 
 }
